24b684207bce2296198c852b311d8178a99a2fc7
Chuck Adams <chaz@chaz.works>
2025-11-02 17:04:40 -0700

    add liberal comments to docker section of flake.nix
    Signed-off-by: Chuck Adams <chaz@chaz.works>

--- flake.nix
+++ flake.nix
@@ -34,17 +34,45 @@
         };
 
         # WIP docker support
-        # https://community.flake.parts/haskell-flake/docker
+        # https://nixos.org/manual/nixpkgs/stable/#ssec-pkgs-dockerTools-buildImage
+        # https://ryantm.github.io/nixpkgs/builders/images/dockertools/ (older but not hideous)
         packages = {
           dockerImage = pkgs.dockerTools.buildImage {
             name = "aspirebuild";
             tag = builtins.substring 0 9 (self.rev or "dev"); # tag with git revision, or 'dev' if dirty
+
+            # created = "now";   # defaults to 1 (that is, 1 second past the epoch). 'now' is not binary-reproducible
+
+
             config = {
+              # https://community.flake.parts/haskell-flake/docker
               Env = [
                 "SSL_CERT_FILE=${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt"
                 "SYSTEM_CERTIFICATE_PATH=${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt"
               ];
+              # Cmd = [ "/path/to/command" ];
+              # WorkingDir = "/data";
+              # Volumes = { "/data" = { }; };
             };
+
+            # tag = "sometag"     # default null = nix output hash
+            #
+            # fromImage = "/path/to/repository-tarball.tgz";  # default null ~ 'FROM scratch'
+            # fromImageName = null;                           # default null = first image in repo tarball
+            # fromImageTag = "latest";                        # default null = first tag for the base image
+            # diskSize = 1024;          # default 1024 (always in MiB)
+            # buildVMMemorySize = 512;  # default 512  (always in MiB)
+            #
+            # copyToRoot = pkgs.buildEnv {
+            #   name = "image-root";
+            #   paths = [ pkgs.redis ];
+            #   pathsToLink = [ "/bin" ];
+            # };
+            #
+            # runAsRoot = ''
+            # #!${pkgs.runtimeShell}
+            # mkdir -p /data
+            # '';
           };
         };
       }
