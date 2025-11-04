710b5c14a4cde618fa1be67db15bbe528c69fa87
Chuck Adams <chaz@chaz.works>
2025-11-04 11:59:26 -0700

    update algorithm for finding/creating $ASPIRESYNC
    Signed-off-by: Chuck Adams <chaz@chaz.works>

--- tools/_common/lib/bash/prelude.bash
+++ tools/_common/lib/bash/prelude.bash
@@ -1,5 +1,18 @@
 # This file should be sourced, not run
 
+#
+# Relevant Environment Variables:
+#
+#   ASPIREBUILD               absolute path to current builder's .aspirebuild directory or symlink
+#                             In a root builder, the .aspirebuild link points to the absolute path of the root.
+#
+#   ASPIREBUILD_DEPTH         current depth of recursive builders
+#
+#   ASPIREBUILD_DEPTH_LIMIT   maximum depth of recursive builders (default 10, including the root)
+#
+#   ASPIREBUILD_BUILDER_DIR   absolute path to location of new builders.  defaults to $ASPIREBUILD/builders
+#
+
 set -o errexit
 set -o nounset
 set -o pipefail
@@ -7,40 +20,55 @@ set -o pipefail
 # These are not exported, but will be visible in the tool if they wish to do so.
 __ORIG_PWD=$PWD
 __HERE=$(dirname "$0")
-__HERE=$(realpath "$__HERE")
+__HERE=$(realpath -s "$__HERE")   # canonicalize only, don't resolve symlinks
 
 function warn  { echo "$@" >&2; }
 function die   { warn "$@"; exit 1; }
 
-function _find_aspirebuild_root {
+function _find_or_create_dot_aspirebuild {
     local dir
     dir=$(dirname "$0")
-    dir=$(realpath "$dir")
+    dir=$(realpath -s "$dir")    # like $__HERE, in that we don't chase symlinks
     while [[ -n $dir ]]; do
-        if [[ -d "$dir/tools/_common" ]]; then
-            echo "$dir"
+        local dot_aspirebuild="$dir/.aspirebuild"
+
+        if [[ -d "$dot_aspirebuild" ]]; then
+            dir=$(realpath "$dir")
+            echo "$dir/.aspirebuild"
             return
+        elif [[ -d "$dir/tools/_common/lib" ]]; then
+            dir=$(realpath "$dir")
+            ln -s "$dir" "$dot_aspirebuild"
+            echo "$dir/.aspirebuild"
+            return
+        else
+            newdir=$(dirname "$dir")
+            [[ $newdir = "$dir" ]] && break
+            dir=$newdir
         fi
-        newdir=$(dirname "$dir")
-        [[ $newdir = "$dir" ]] && break
-        dir=$newdir
     done
     die "Could not find aspirebuild root in any parent directory of $PWD"
 }
 
 # spawned builders will set this to the new builder's .aspirebuild dir
-export ASPIREBUILD=${ASPIREBUILD:-$(_find_aspirebuild_root)}
+export ASPIREBUILD=${ASPIREBUILD:-$(_find_or_create_dot_aspirebuild)}
 export ASPIREBUILD_DEPTH=$(( ${ASPIREBUILD_DEPTH:--1} + 1 )) # base is level 0, meaning builders will be at level 1
 
-[[ $ASPIREBUILD_DEPTH -lt ${ASPIREBUILD_RECURSION_LIMIT:-10} ]] || die "Maximum aspirebuild recursion depth reached.  Aborted."
+[[ $ASPIREBUILD_DEPTH -lt ${ASPIREBUILD_DEPTH_LIMIT:-10} ]] || die "Maximum aspirebuild recursion depth reached.  Aborted."
 
-# We bail out early if our cwd contains spaces, rather than risk stepping on this mine later.
+# We bail out early if our working directory contains spaces, rather than risk stepping on this mine later.
 # We make reasonable efforts to quote bash arguments, but 'bash' and 'reasonable' do not belong in the same sentence.
-[[ "$ASPIREBUILD" =~ [[:space:]] ]] && die "Refusing to deal with working directory containing whitespace.  Aborted."
+[[ "$ASPIREBUILD" =~ [[:space:]] ]] && die "Refusing to deal with aspirebuild directory containing whitespace.  Aborted."
 
 cd "$ASPIREBUILD"
 
-for file in $(shopt -s nullglob; echo "$__HERE/../lib/bash/prelude.d"/*.bash); do
+# Run all prelude files under the current tool's lib (usually symlinked to tools/_common/lib)
+preludes=$(
+    shopt -s nullglob
+    echo "$__HERE"/../lib/bash/prelude.d/*.bash "$__HERE"/../local/lib/bash/prelude.d/*.bash
+);
+
+for file in $preludes; do
     # shellcheck source=/dev/null
-    source "$file"
+    [[ -f $file ]] && source "$file"
 done
