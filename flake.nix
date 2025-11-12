710f23ee94c112419286910a15267162b5a8b964
Chuck Adams <chaz@chaz.works>
2025-11-12 11:34:59 -0700

    build all needed extensions
    Signed-off-by: Chuck Adams <chaz@chaz.works>

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  # a work in progress.  only devshell is supported for now.
+  # a work in progress.
 
   description = "AspireBuild";
 
@@ -44,8 +44,6 @@
             lrzip
             perl
             php
-            php84Extensions.ffi
-            php84Extensions.intl
             php84Packages.composer
             subversion
             sqlite
@@ -55,6 +53,53 @@
             zip
             zstd
           ];
+
+          extensions = with pkgs.php84Extensions; [
+            bcmath
+            curl
+            ffi
+            filter
+            gettext
+            gmp
+            intl
+            mbstring
+            pdo
+            pdo_sqlite
+            readline
+            sockets
+            sodium
+            sqlite3
+
+            # full list in /nix/store/nsybw5k5jcqwccbgslfq5psmqh3x3svs-php-with-extensions-8.4.14/lib/php.ini
+
+            # future consideration
+            #ctype
+            #dom
+            #fileinfo
+            #iconv
+            #mysqli
+            #mysqlnd
+            #openssl
+            #pcntl
+            #pdo_mysql
+            #pdo_pgsql
+            #pgsql
+            #posix
+            #session
+            #simplexml
+            #sockets
+            #tokenizer
+            #xmlreader
+            #xmlwriter
+            #zip
+            #zlib
+          ];
+
+          zend-extensions = with pkgs.php84Extensions; [
+            opcache
+            xdebug
+          ];
+
         in
         {
           devShells.default = pkgs.mkShell {
@@ -64,7 +109,8 @@
 
             shellHook = ''
               export ASPIREBUILD=$FLAKE_ROOT
-              export PHP_INI_SCAN_DIR=:${self'.packages.default}/.php-ini
+              export SELF_DIR=${self'.packages.default}
+              export PHP_INI_SCAN_DIR=:${self'.packages.default}
             '';
 
             # in case $FLAKE_ROOT isn't available, this should also work
@@ -78,11 +124,22 @@
 
             src = ./.;
 
-            buildPhase = ''
-              mkdir -p $out/.php-ini
+            php-ini =
+              let
+                get-extension-name = name: builtins.elemAt (builtins.split "-" name) 2; # "php-intl-8.4.13" -> "intl"
+                ext-line = ext: "extension = ${ext}/lib/php/extensions/${get-extension-name ext.name}.so";
+                zend-ext-line = ext: "zend_extension = ${ext}/lib/php/extensions/${get-extension-name ext.name}.so";
+              in
+              (map ext-line extensions) ++ (map zend-ext-line zend-extensions);
 
-              echo "extension=${pkgs.php84Extensions.ffi}/lib/php/extensions/ffi.so" > $out/.php-ini/php.ini
-              echo "extension=${pkgs.php84Extensions.intl}/lib/php/extensions/intl.so" > $out/.php-ini/php.ini
+            buildPhase = ''
+              mkdir -p $out
+
+              cat << EOF > $out/php.ini
+              ; PHP extensions for AspireBuild
+
+              ${lib.concatStringsSep "\n" config.packages.default.php-ini}
+              EOF
             '';
 
             installPhase = "true"; # if installPhase is absent or blank, it defaults to 'just' for some reason
